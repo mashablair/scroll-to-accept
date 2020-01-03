3617689c3979827ee6136573f215ff297c90959e
Maria Blair <mashablair@gmail.com>
2020-01-03 11:23:56 -0800

    update JS
    Add old way with a scroll event listener and the new way with Intersection Observer.

--- scroll-to-accept.js
+++ scroll-to-accept.js
@@ -1,1 +1,29 @@
-console.log('IT WORKS!');
+const terms = document.querySelector(".terms-and-conditions");
+const button = document.querySelector(".accept");
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+
+    // stop observing
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, { root: terms, threshold: 1 });
+
+ob.observe(terms.lastElementChild);
+
+// The old way to do it:
+// function scrollToAccept() {
+
+//   // if no terms elem, bail
+//   if (!terms) return;
+
+//   terms.addEventListener("scroll", function(e) {
+//     console.log(e.currentTarget.scrollTop);
+//     console.log(e.currentTarget.scrollHeight);
+//   });
+// }
+
+// scrollToAccept();
